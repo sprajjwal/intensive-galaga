31a667c833274998b36338ae04a9c3d8d4979389
sprajjwal <shashwatprajjwal@gmail.com>
2020-03-18 11:14:07 -0700

    init v3

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(expressValidator());
 // enable JWT
 app.use(cookieParser());
 
-// app.get('/', (req, res) => res.render(''))
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
